c771e738479de864dbd50c1ea20ef5c83a8647ac
topikhidayatt <topik.webin@gmail.com>
2021-05-25 00:40:08 +0700

    Add: Placeholder Data

---
+++ lib/json/home_page_data.dart
@@ -0,0 +1,60 @@
+const List category = [
+  "Indomie Kuah",
+  "Indomie Goreng",
+  "Snack",
+  "Softdrink",
+];
+
+const List menu = [
+  {
+    "img": "",
+    "name": "Indomie Goreng with red chillies",
+    "price": "35000",
+    "rate": "4.5"
+  },
+  {
+    "img": "",
+    "name": "Indomie Kuah with curry",
+    "price": "25000",
+    "rate": "4.5"
+  },
+  {
+    "img": "",
+    "name": "Indomie Kuah soto lamongan",
+    "price": "35000",
+    "rate": "4.5"
+  },
+  {
+    "img": "",
+    "name": "Indomie Kuah Baso",
+    "price": "35000",
+    "rate": "4.5",
+  },
+  {
+    "img": "",
+    "name": "Indomie Goreng Ayam",
+    "price": "35000",
+    "rate": "4.5",
+  },
+  {
+    "img": "",
+    "name": "Nasi Goreng Pak Haji Asep",
+    "price": "35000",
+    "rate": "4.5"
+  },
+  {
+    "img": "",
+    "name": "Nasi Uduk Bah ocim",
+    "price": "35000",
+    "rate": "4.5",
+  },
+];
+
+const List toping = [
+  {"img": "", "name": "Telur"},
+  {"img": "", "name": "Sate"},
+  {"img": "", "name": "Bala - Bala"},
+  {"img": "", "name": "Tahu"},
+  {"img": "", "name": "Tempe"},
+  {"img": "", "name": "Sayur"},
+];
